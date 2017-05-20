d5d1b68be0076cd38294007f105c0cb598566a4f
Zhen <zhl146@gmail.com>
2017-05-20 17:57:01 -0400

    removed leftover code from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  // currently controls which view is active: recipe book or shopping list
-  // there is only one boolean because they are mutually exclusive
-  // will most likely change this to routing instead of ngIf later
-  recipeActive: boolean = true;
-
-  // reacts to a click event in the header component
-  // records the boolean passed by the event in order to change the view via ngIf
-  onLinkClicked(data: boolean) {
-    this.recipeActive = data;
-  }
 }
