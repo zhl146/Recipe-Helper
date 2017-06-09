8c5cf3b23d4dbe8ff1dce1ca47ca704359a33b45
Zhen <zhl146@gmail.com>
2017-06-09 12:37:18 -0400

    added layout styling for larger resolutions

--- src/app/auth/password-reset/password-reset.component.ts
+++ src/app/auth/password-reset/password-reset.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 
@@ -8,39 +7,13 @@ import { Router } from '@angular/router';
   templateUrl: './password-reset.component.html',
   styleUrls: ['../auth.component.scss']
 })
-export class PasswordResetComponent implements OnInit {
-
-  resetForm: FormGroup;
+export class PasswordResetComponent {
 
   constructor( private auth: AuthService,
                private router: Router ) { }
 
-  // initialize the form
-  // both controls are required
-  ngOnInit() {
-    this.resetForm = new FormGroup({
-      code: new FormControl(''),
-
-      // firebase requires the password to be at least 6 characters long
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
-    });
-  }
-
-  passwordValid() {
-    return ( this.resetForm.get('password').valid );
-  }
-
-  // we don't want to start by displaying an error message to the user
-  // let the user actually do something first
-  passwordTouched() {
-    return (this.resetForm.get('password').touched &&
-    this.resetForm.get('password').dirty );
-  }
-
   // submits the form
-  onSubmit() {
-    const code = this.resetForm.get('code').value;
-    const password = this.resetForm.get('password').value;
-    this.auth.createNewPassword(code, password);
+  goToSignIn() {
+    this.router.navigate(['auth', 'signin']);
   }
 }
